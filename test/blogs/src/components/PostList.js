3dc9290c16d1f9451390f0954070116480c59d60
Chandra Kant <chandrakant@gytworkz.com>
2022-05-06 14:37:15 -0500

    blog init

---
+++ test/blogs/src/components/PostList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+class PostList extends React.Component {
+
+    render() {
+        return (
+            <div className='ui container'>
+                PostList
+            </div>
+        );
+    }
+}
+
+export default PostList;
